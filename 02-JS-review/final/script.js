1dbafbb44059ceb20353c38ceb3e135a3347e5d2
ELEOMAR <kaotikus27@gmail.com>
2026-01-21 12:40:00 +0800

    test

--- 02-JS-review/final/script.js
+++ 02-JS-review/final/script.js
@@ -297,10 +297,7 @@ booksAfterUpdate;
 
 */
 
-// fetch("https://jsonplaceholder.typicode.com/todos")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
-
+//
 // console.log("jonas");
 
 async function getTodos() {
