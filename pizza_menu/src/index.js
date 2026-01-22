90a017bb473395c43acb393f1d36c0921223f323
ELEOMAR <kaotikus27@gmail.com>
2026-01-22 11:36:52 +0800

    pizza

--- pizza_menu/src/index.js
+++ pizza_menu/src/index.js
@@ -48,14 +48,47 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>hello!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
     </div>
   );
 }
 
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
+  );
+}
+
 function Pizza() {
-  return <h2>PIZAA</h2>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  else alert("Closed");
+  console.log(hour);
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
