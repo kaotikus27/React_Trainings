5453631a7775257e9e445a5764eaf3931c21c8e3
ELEOMAR <kaotikus27@gmail.com>
2026-01-26 11:36:12 +0800

    pizza

--- pizza_menu/src/index.js
+++ pizza_menu/src/index.js
@@ -95,8 +95,6 @@ function Menu() {
 }
 
 function Pizza({ pizzaObj }) {
-  //   if (pizzaObj.soldOut) return null;
-
   return (
     <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
