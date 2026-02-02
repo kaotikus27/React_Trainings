a3c8e4a41e79770a678f24b03cfbc30ec99e48e5
ELEOMAR <kaotikus27@gmail.com>
2026-02-02 13:07:49 +0800

    test CRUD

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,11 +7,19 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {}
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
 
       {/* <FlashCards /> */}
@@ -23,7 +31,7 @@ function Logo() {
   return <h1>🌴 Far Away 💼 </h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -32,8 +40,8 @@ function Form() {
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-
     console.log(newItem);
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -63,11 +71,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} />
         ))}
       </ul>
